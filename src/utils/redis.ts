7afe5e7b32a311590c7f2b379bc4a1cf910318dc
asfalfa <glkin9@gmail.com>
2024-11-15 10:21:14 +0100

    feat: add redis

---
+++ src/utils/redis.ts
@@ -0,0 +1,8 @@
+import { Redis } from "@upstash/redis";
+
+const redis = new Redis({
+  url: import.meta.env.UPSTASH_REDIS_REST_URL,
+  token: import.meta.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+export default redis;
