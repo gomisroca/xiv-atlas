b1cb5076a7914f77667056f141cce19d575eea88
asfalfa <glkin9@gmail.com>
2024-11-21 11:06:23 +0100

    fix: remove instance from possibly having icon

--- src/pages/content/[type]/getters.ts
+++ src/pages/content/[type]/getters.ts
@@ -15,12 +15,7 @@ export function getBanner(item: FormattedContent): string | undefined {
 }
 
 export function getIcon(item: FormattedContent): string | undefined {
-  if (
-    isAchievement(item) ||
-    isItem(item) ||
-    isInstance(item) ||
-    isAction(item)
-  ) {
+  if (isAchievement(item) || isItem(item) || isAction(item)) {
     return item.icon;
   }
   return undefined;
