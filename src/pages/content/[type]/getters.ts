a32eecdcba73ed4e0e1d6d8bbe9da87ef06564f3
asfalfa <glkin9@gmail.com>
2024-11-22 12:24:07 +0100

    feat: add type-specific details to cards

--- src/pages/content/[type]/getters.ts
+++ src/pages/content/[type]/getters.ts
@@ -32,3 +32,80 @@ export function getDescription(item: FormattedContent): string | undefined {
   }
   return undefined;
 }
+
+interface QuestUtils {
+  location: string;
+  npc: string;
+}
+export function getQuestUtils(item: FormattedContent): QuestUtils | undefined {
+  if (isQuest(item)) {
+    return {
+      location: item.location,
+      npc: item.npc,
+    };
+  }
+  return undefined;
+}
+
+interface InstanceUtils {
+  levelRequired: number;
+  levelSync: number;
+}
+export function getInstanceUtils(
+  item: FormattedContent,
+): InstanceUtils | undefined {
+  if (isInstance(item)) {
+    return {
+      levelRequired: item.levelRequired,
+      levelSync: item.levelSync,
+    };
+  }
+  return undefined;
+}
+
+interface ItemUtils {
+  category: string;
+  hq: boolean;
+  itemLevel: number;
+  equipLevel: number;
+  rarity: number;
+  materiaSlotCount: number;
+  glamour: boolean;
+  unique: boolean;
+  stats: object;
+}
+export function getItemUtils(item: FormattedContent): ItemUtils | undefined {
+  if (isItem(item)) {
+    return {
+      category: item.category,
+      hq: item.hq,
+      itemLevel: item.itemLevel,
+      equipLevel: item.equipLevel,
+      rarity: item.rarity,
+      materiaSlotCount: item.materiaSlotCount,
+      glamour: item.glamour,
+      unique: item.unique,
+      stats: item.stats,
+    };
+  }
+  return undefined;
+}
+
+interface AchievementUtils {
+  category: string;
+  title: {
+    masculine: string;
+    feminine: string;
+  };
+}
+export function getAchievementUtils(
+  item: FormattedContent,
+): AchievementUtils | undefined {
+  if (isAchievement(item)) {
+    return {
+      category: item.category,
+      title: item.title,
+    };
+  }
+  return undefined;
+}
