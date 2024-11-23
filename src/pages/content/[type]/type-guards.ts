7088d6e3478d406840fa2458a49fe29557ca89bd
asfalfa <glkin9@gmail.com>
2024-11-23 09:54:18 +0100

    feat: improve item typeguard

--- src/pages/content/[type]/type-guards.ts
+++ src/pages/content/[type]/type-guards.ts
@@ -12,7 +12,22 @@ export function isAchievement(item: FormattedContent): item is Achievement {
 }
 
 export function isItem(item: FormattedContent): item is Item {
-  return "itemLevel" in item && "icon" in item && "description" in item;
+  return (
+    typeof item === "object" &&
+    item !== null &&
+    "itemLevel" in item &&
+    typeof item.itemLevel === "number" &&
+    "equipLevel" in item &&
+    typeof item.equipLevel === "number" &&
+    "rarity" in item &&
+    typeof item.rarity === "number" &&
+    "materiaSlotCount" in item &&
+    typeof item.materiaSlotCount === "number" &&
+    "icon" in item &&
+    typeof item.icon === "string" &&
+    "description" in item &&
+    typeof item.description === "string"
+  );
 }
 
 export function isQuest(item: FormattedContent): item is Quest {
